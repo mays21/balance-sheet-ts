f880b7dea19c603643bd01ddcb04e6f254346f9f
mays21 <mayu6_6@hotmail.com>
2024-06-12 21:41:05 +0900

    materialUI設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Report from './pages/Report'
+import NotFound from './pages/NotFound'
+import AppLayout from './components/layout/AppLayout'
 
 function App() {
 
   return (
-    <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route path='/report' element={<Report />} />
+        <Route path='/*' element={<NotFound />} />
+        </Route>
 
-    </div>
+      </Routes>
+
+    </Router>
   )
 }
 
